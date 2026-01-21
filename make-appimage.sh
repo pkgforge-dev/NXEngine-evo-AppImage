cc5c3abfa6f7ac30345e8519544dc80cca153f63
Link4Electronics <link4electronics@gmail.com>
2026-01-21 06:02:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,8 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/org.nxengine.nexneing_evo.png
-export DESKTOP=/usr/share/applications/org.nxengine.nexneing_evo.desktop
+export ICON=/usr/share/icons/hicolor/256x256/apps/org.nxengine.nxengine_evo.png
+export DESKTOP=/usr/share/applications/org.nxengine.nxengine_evo.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
