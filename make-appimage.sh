f745b6edc1d94e490d6009f6be4b8232371dd524
Link4Electronics <link4electronics@gmail.com>
2026-01-23 21:11:56 -0300

    remove pipewire

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/256x256/apps/org.nxengine.nxengine_evo.png
 export DESKTOP=/usr/share/applications/org.nxengine.nxengine_evo.desktop
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/nxengine-evo
