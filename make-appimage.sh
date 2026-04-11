d1d23e093575149b28007fc1bbf6e5c793eafab8
Link4Electronics <link4electronics@gmail.com>
2026-04-11 20:34:43 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q nxengine-evo-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export APPNAME="Cave Story NX-evo"
 export ICON=/usr/share/icons/hicolor/256x256/apps/org.nxengine.nxengine_evo.png
